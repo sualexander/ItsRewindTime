70de52f0f57c64df49a3f538e69d75a828d89e4f
Alexander Su <sualexander2013@gmail.com>
2024-09-08 11:36:05 -0400

    setup

---
+++ Plugins/RewindCodePlugin/Source/RewindCodePlugin/Private/RewindCodeModule.cpp
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RewindCodeModule.h"
+
+#define LOCTEXT_NAMESPACE "FRewindCodeModule"
+
+void FRewindCodeModule::StartupModule()
+{
+}
+
+void FRewindCodeModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FRewindCodeModule, RewindCodePlugin)
---
+++ Plugins/RewindCodePlugin/Source/RewindCodePlugin/RewindCodePlugin.Build.cs
@@ -0,0 +1,48 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class RewindCodePlugin : ModuleRules
+{
+	public RewindCodePlugin(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PublicIncludePaths.AddRange(
+			new string[] {
+			}
+			);
+				
+		
+		PrivateIncludePaths.AddRange(
+			new string[] {
+			}
+			);
+			
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+			}
+			);
+			
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"SlateCore",
+			}
+			);
+		
+		
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+			}
+			);
+	}
+}
