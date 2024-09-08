3554e823f02d282be6b07ed2823e5ed6a4961191
Alexander Su <sualexander2013@gmail.com>
2024-09-08 11:51:58 -0400

    more setup

--- Plugins/RewindCodePlugin/Source/RewindCodePlugin/Public/RewindCodeModule.h
+++ Plugins/RewindCodePlugin/Source/RewindCodePlugin/Public/RewindCodeModule.h
@@ -5,6 +5,8 @@
 #include "CoreMinimal.h"
 #include "Modules/ModuleManager.h"
 
+#include "RewindCodeModule.generated.h"
+
 class FRewindCodeModule : public IModuleInterface
 {
 public:
@@ -12,4 +14,17 @@ public:
 	/** IModuleInterface implementation */
 	virtual void StartupModule() override;
 	virtual void ShutdownModule() override;
+};
+
+UCLASS()
+class UTest : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	static void PluginFunction()
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Noice"));
+	}
 };
